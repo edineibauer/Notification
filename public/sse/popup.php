78fd60221f49744327f385510a983143941bc38d
Edinei J. Bauer <edineibauer@gmail.com>
2020-12-09 15:28:50 -0300

    popup check null ownerpubs

--- public/sse/popup.php
+++ public/sse/popup.php
@@ -3,7 +3,7 @@
 $data['data'] = [];
 if(!empty($_SESSION['userlogin']) && $_SESSION['userlogin']['id'] > 0) {
     $sql = new \Conn\SqlCommand();
-    $sql->exeCommand("SELECT * FROM " . PRE . "popup WHERE data_de_exibicao <= NOW() AND (ownerpub = {$_SESSION['userlogin']['id']} OR (ownerpub = 1 AND id NOT IN (SELECT popup FROM ".PRE."popup_user WHERE popup IS NOT NULL AND ownerpub = {$_SESSION['userlogin']['id']})))", !0);
+    $sql->exeCommand("SELECT * FROM " . PRE . "popup WHERE data_de_exibicao <= NOW() AND (ownerpub = {$_SESSION['userlogin']['id']} OR (ownerpub IS NULL AND id NOT IN (SELECT popup FROM ".PRE."popup_user WHERE popup IS NOT NULL AND ownerpub = {$_SESSION['userlogin']['id']})))", !0);
     if($sql->getResult()) {
         foreach ($sql->getResult() as $item) {
             $item['imagem'] = !empty($item['imagem']) ? json_decode($item['imagem'], !0) : [];
