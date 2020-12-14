07a3c22bd5efe741b980e8159ed0f281d6bf60f8
Edinei J. Bauer <edineibauer@gmail.com>
2020-12-14 15:11:50 -0300

    push notification update subscription

--- public/post/pushNotificationRegister.php
+++ public/post/pushNotificationRegister.php
@@ -2,6 +2,14 @@
 
 $token = filter_input(INPUT_POST, 'tokenPush', FILTER_DEFAULT);
 $code = filter_input(INPUT_POST, 'code', FILTER_DEFAULT);
+$code = $code ?? "FCM";
 
-$create = new \Conn\Create();
-$create->exeCreate("push_notifications", ["subscription" => $token, "usuario" => $_SESSION['userlogin']['id'], "code" => $code ?? "FCM", "system_id" => null]);
+$read = new \Conn\Read();
+$read->exeRead("push_notifications", "WHERE usuario = {$_SESSION['userlogin']['id']} AND code = '{$code}'");
+if($read->getResult()) {
+    $up = new \Conn\Update();
+    $up->exeUpdate("push_notifications", ["subscription" => $token], "WHERE id = :id", "id={$read->getResult()[0]['id']}");
+} else {
+    $create = new \Conn\Create();
+    $create->exeCreate("push_notifications", ["subscription" => $token, "usuario" => $_SESSION['userlogin']['id'], "code" => $code, "system_id" => null]);
+}
